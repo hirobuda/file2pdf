8bd7ef007ed54fa65bc8588b11337fe0272afe12
Hiroshi Ito <49350438+hirobuda@users.noreply.github.com>
2022-03-03 22:47:55 -0300

    Add files via upload

---
+++ file2pdf.py
@@ -0,0 +1,30 @@
+from PIL import Image as img
+from PIL import ImageFile
+from os import listdir
+from natsort import natsorted
+import sys
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+def list_images(path, ext):
+    list = listdir(str(path))
+    for i in range(len(list)):
+        list[i] = path+'/'+list[i]
+    return natsorted(list)
+
+while True:
+    file = input("file with the images: ")
+    ext = input("image extension: ")
+
+    imagens = list_images(str(file),ext)
+    to_pdf = []
+
+    img1 = imagens[0]
+    for i in range(len(imagens)):
+        to_pdf.append(img.open(imagens[i]))
+
+    for i in range(len(to_pdf)):
+        to_pdf[i] = to_pdf[i].resize((1700,2400), resample=img.ANTIALIAS)
+    pdf1_filename = input("nome do mangao final: ")
+    to_pdf[0].save(pdf1_filename, "PDF" ,resolution=100.0, save_all=True, append_images = to_pdf)
+
