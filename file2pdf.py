e91922bc5e261861dc38ff6cfe8eedc978da521a
Hiroshi Ito <49350438+hirobuda@users.noreply.github.com>
2022-04-11 11:47:50 -0300

    Simpler usage and resize improvement.

--- file2pdf.py
+++ file2pdf.py
@@ -1,30 +1,37 @@
 from PIL import Image as img
 from PIL import ImageFile
 from os import listdir
+from os import path
 from natsort import natsorted
+
 import sys
+import numpy as np
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-def list_images(path, ext):
+def list_images(path):
     list = listdir(str(path))
     for i in range(len(list)):
-        list[i] = path+'/'+list[i]
+        if list[i].split('.')[1] in ["jpg", "png"]:
+            list[i] = path+'/'+list[i]
     return natsorted(list)
 
 while True:
     file = input("file with the images: ")
-    ext = input("image extension: ")
-
-    imagens = list_images(str(file),ext)
+    
+    images = list_images(str(file))
     to_pdf = []
 
-    img1 = imagens[0]
-    for i in range(len(imagens)):
-        to_pdf.append(img.open(imagens[i]))
+    img1 = images[0]
+    for i in range(len(images)):
+        to_pdf.append(img.open(images[i]))
 
     for i in range(len(to_pdf)):
-        to_pdf[i] = to_pdf[i].resize((1700,2400), resample=img.ANTIALIAS)
-    pdf1_filename = input("pdf name: ")
+        width, height = to_pdf[i].width, to_pdf[i].height
+        to_pdf[i] = to_pdf[i].resize((int(np.ceil(width*1.74)), int(np.ceil(height*1.41))), resample=img.Resampling.LANCZOS)
+    pdf1_filename = input("final location: ")
     to_pdf[0].save(pdf1_filename, "PDF" ,resolution=100.0, save_all=True, append_images = to_pdf)
+    
+    to_pdf.clear()
+    images.clear()
 
